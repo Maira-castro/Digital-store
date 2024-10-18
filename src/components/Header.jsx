fac0fdd0691438370884437c90c32eee66cc3301
JesusEduardo <edu174580@gmail.com>
2024-10-18 19:55:52 -0300

    git repositorio

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-top">
+        <div className="">
+            <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/logo-header.svg" alt="Logo-header-top"/>
+        <input type="search" />
+        <i></i></div>
+        <div className="cad-button">
+          <a href="">Cadastre-se</a>
+          <button>Entrar</button>
+        </div>
+        <img src="./src/assets/mini-cart.svg" alt="mini-cart" />
+      </div>
+      <div className="menu-nav">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="">Produtos</a>
+          </li>
+          <li>
+            <a href="">Categorias</a>
+          </li>
+          <li>
+            <a href="">Meus Pedidos</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/paginas/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Home = () => {
+    return(
+        <section className="Gallery">
+            <h6>Melhores ofertas personalizadas</h6>
+            <h1>Queima de<br/> stoque Nike 🔥</h1>
+            <p>Consequat culpa exercitation mollit nisi excepteur do <br/> do tempor laboris eiusmod irure consectetur.</p>
+            <button>Ver Ofertas</button>
+            <img src="./src/assets/home-img.svg" alt="foto-tenis-nikea-air" />
+        </section>
+    )
+}
+
+export default Home;
